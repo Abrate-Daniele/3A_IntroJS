8f3b190a419ff22ad9c7495ac98ddf5e874afd4c
Daniele Abrate <abrate.daniele2007@vallauri.edu>
2024-03-01 12:13:11 +0100

    salvataggio

--- pag2.js
+++ pag2.js
@@ -1,5 +1,16 @@
-var txtUtente = document.getElementById("txtUtente");
-console.log(txtUtente);
-document.write("Salve Mondo");
-var btnAccedi = document.getElementById("btnAccedi");
-btnAccedi.innerHTML = "LOGIN";
+document.write("SALVE MONDO");
+function init(){
+    var txtUtente = document.getElementById("txtUtente");
+    console.log(_txtUtente);
+    txtUtente.placeholder = "Inserisci utente";
+    var btnAccedi = document.getElementById("btnAccedi");
+    btnAccedi.innerHTML = "LOGIN";
+}
+function accedi(){
+    let utente = "daniele.fea", pwd = "test";
+    let txtUtente = document.getElementById("txtUtente");
+    if(txtUtente.value == utente)
+        alert("Login effettuato");
+    else
+        alert("Errore nelle credenziali");
+}
